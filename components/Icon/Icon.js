db9ed2f5331cfe8cca2713a27f8e34097dadfa94
dominicom <dominik.kiepuszewski@gmail.com>
2022-02-14 18:37:53 +0100

    Changed icon pack to 'react-feather'

--- components/Icon/Icon.js
+++ components/Icon/Icon.js
@@ -1,39 +1,20 @@
-import { SWMIcon } from 'react-swm-icon-pack';
 
-import parse from "html-react-parser";
-import feather from "feather-icons";
+import React from 'react';
 
+import * as FeatherIconPack from 'react-feather'
 
+const IconComponent = ({ type, settings }) => {
+  return React.createElement(FeatherIconPack[type], settings);
+}
 
-// export default function Icon({ name = "circle", className, stroke }) {
-//   const iconName = name || "circle";
+export default function Icon({ name = 'Circle', className, size, stroke }) {
 
-//   const IconComponent = feather.icons[name].toSvg({
-//     class: className || "",
-//     "stroke-width": stroke || 1,
-//     color: "currentColor",
-//     width: 16,
-//     height: 16
-//   });
+  const attributes = {
+    class: className || '',
+    strokeWidth: stroke || 2,
+    color: "currentColor",
+    size: size || 24
+  }
 
-//   return parse(IconComponent);
-// }
-
-
-
-
-export default function Icon({ className, name, stroke, size, color, set }) {
-  return(
-    <span className={className || 'icon'}>
-      <SWMIcon
-        name={name || 'Circle'} 
-        strokeWidth={stroke || 1}
-        size={size || 24}
-        color={color || 'currentColor'} 
-        set={set || 'oultine'} 
-      />
-    </span>
-
-  )  
-
-};
+  return <IconComponent type={name} settings={attributes} /> 
+}
